b5b974ecda649d78cc3bf9da708f2e365b6df849
aneeeeesha <84973812+aneeeeesha@users.noreply.github.com>
2022-07-19 11:36:59 +0530

    Update Supplier_Lab3.sql

--- Supplier_Lab3.sql
+++ Supplier_Lab3.sql
@@ -1,24 +1,87 @@
-create database lab_3;
-use lab_3;
+create database supplier_db;
+use supplier_db;
+create table SUPPLIERS(sid int,sname varchar(20),address varchar(30), PRIMARY KEY(sid));
+create table PARTS(pid int,pname varchar(20),color varchar(20), PRIMARY KEY(pid));
+create table CATALOG(sid int,pid int,cost real, PRIMARY KEY(sid,pid),FOREIGN KEY(sid) REFERENCES SUPPLIERS(sid),FOREIGN KEY(pid) REFERENCES PARTS(pid));
+desc SUPPLIERS;
+desc PARTS;
+desc CATALOG;
+insert into SUPPLIERS(sid ,sname,address) 
+			values ("S0123","Acme Widget","Bengaluru"),
+			       ("S0234","Finolex","Bengaluru"),
+			       ("S0456","Johnsons","Chennai"),
+                   	       ("S0456","Niral","Hyderabad"),
+                   	       
+                   
+insert into PARTS(pid ,pname,color) 
+			values ("P0456","Hammer","Red"),
+			       ("P0456","Spanner","Red"),
+			       ("P0888","Wedge","Green"),
+                               ("P0998","Screwdriver","Green"),
+                               ("P0999","Brush","Yellow");
+                   
+insert into CATALOG(sid ,pid,cost) 
+			values ("S0123","P0456",3000),
+			       ("S0123","P0457",2000),
+			       ("S0123","P0888",1000),
+			       ("S0123","P0998",1000),
+			       ("S0123","P0999",1000),
+			       ("S0234","P0456",3100),
+			       ("S0234","P0457",2200),
+			       ("S0456","P0888",4000),
+			       ("S0456","P0999",5000),
+			       ("S0789","P0999",4000);
+                  
+                   
+commit;
 
-create table Suppliers(sid int, sname varchar(20), address varchar(20), primary key(sid));
-create table Parts(pid int, pname varchar(20), color varchar(20), primary key(pid));
-create table Catalog(sid int, pid int, cost real, foreign key (sid) references Suppliers(sid), foreign key (pid) references Parts(pid));
+select DISTINCT P.pname
+FROM PARTS P,CATALOG C
+WHERE P.pid = C.pid;
 
-insert into Suppliers values(100, 'Widget', 'Bengaluru'),(101,'Watch', 'Kolkata'), (102, 'Phone', 'Mumbai'), (103, 'Fan', 'Chennai');
-update Suppliers set sname='Mahindra' where sid=103;
-select * from Suppliers;
+SELECT S.sname FROM SUPPLIERS S
+WHERE NOT EXISTS 
+(SELECT
+P.pid FROM PARTS P
+WHERE NOT EXISTS
+(SELECT C.pid
+    FROM CATALOG C
+    WHERE C.SID = S.sid)
+    AND C.pid = P.pid
+    )
+    );
+    
+SELECT S.sname FROM SUPPLIERS S
+WHERE NOT EXISTS 
+(SELECT
+P.pid FROM PARTS P
+WHERE NOT EXISTS
+(SELECT C.pid
+    FROM CATALOG C
+    WHERE C.SID = S.sid)
+    AND C.pid = P.pid
+    AND P.color = 'red'));
+    )
+    );
 
-insert into Parts values (200,'Book','Red'),(201,'Pen','Blue'),(202,'Mobile','Gray'),(203,'Charger','White');
-update Parts set pid=203 where pname='Charger';
-insert into Catalog values (100,200,20),(101,201,10),(102,201,30),(102,203,1500),(103,200,3200),(101,203,2500);
-select * from Catalog;
+SELECT P.pname 
+FROM PART P,SUPPLIERS S,CATALOG C
+WHERE P.pid = C.pid AND S.sid = C.sid
+AND S.sname = 'Acme Widget'
+AND NOT EXISTS(SELECT *
+		FROM CATALOG C1,SUPPLIERS S1
+		WHERE P.pid = C1.pid
+		AND C1.sid = S1.sid
+		AND s1.sname <> 'Acme Widget'
 
-select distinct P.pname 
-from Parts P, Catalog C
-where P.pid=C.pid;
-
-select S.sname, count(*) as parts
-from Suppliers S, Catalog C
-where count(*) 
-group by C.sid;
+SELECT C1.sid,c1.cost,c2.sid,c2.cost
+FROM CATALOG C1,CATALOG C2
+WHERE C1.cost > C2.cost AND
+      C1.pid = C2.pid AND
+      C1.sid <> C2.sid;
+      
+SELECT DISTINCT SUPPLIERS.sid
+FROM SUPPLIERS,PARTS,CATALOG
+WHERE SUPPLIERS.sid = CATALOG.SID
+AND PARTS.pid = CATALOG.pid
+AND PARTS.color = 'red';
